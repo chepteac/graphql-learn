a97cd236438cd0c3c1b4453b2ee5366071d5ebf9
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-06-03 11:51:05 -0400

    create express application

---
+++ users/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(4000, () => {
+  console.log('Listening on port 4000');
+});
+
+const expressGraphQL = require('express-graphql').graphqlHTTP;
