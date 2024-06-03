47f3ae71fe867be24acbff4905486ddfe0b75056
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-06-03 12:07:34 -0400

    add schema file

--- users/server.js
+++ users/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql').graphqlHTTP;
 
 const app = express();
+
+app.use(
+  '/graphql',
+  expressGraphQL({
+    graphiql: true,
+  })
+);
 
 app.listen(4000, () => {
   console.log('Listening on port 4000');
 });
-
-const expressGraphQL = require('express-graphql').graphqlHTTP;
