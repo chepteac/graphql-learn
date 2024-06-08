016afdfd684f938d60c4ebca5f43804c4c9fe80b
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-06-08 20:31:13 +0300

    delete mutation

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -74,17 +74,26 @@ const mutation = new GraphQLObjectType({
     addUser: {
       type: UserType,
       args: {
-        firstName: {type: GraphQLNonNull(GraphQLString)},
-        age: {type: GraphQLNonNull(GraphQLInt)},
-        companyId: {type: GraphQLString},
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString }
       },
-      resolve(parentValue, {firstName, age}) {
-        return axios
-          .post('http://localhost:3000/users', {firstName, age})
-          .then(({data}) => data);
-      },
+      resolve(parentValue, { firstName, age, companyId }) {
+        return axios.post('http://localhost:3000/users', { firstName, age, companyId })
+          .then(res => res.data);
+      }
     },
-  },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parentValue, { id }) {
+        return axios.delete(`http://localhost:3000/users/${id}`)
+          .then(res => res.data);
+      }
+    }
+  }
 });
 
 module.exports = new GraphQLSchema({query: RootQuery, mutation});
