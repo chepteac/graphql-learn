56653371e4238f9c9e2407fc680aee64fa361677
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-06-14 05:23:14 -0400

    old: Mutations in React

--- Lyrical-GraphQL/client/components/SongCreate.js
+++ Lyrical-GraphQL/client/components/SongCreate.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import gql from 'graphql-tag';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -7,11 +8,15 @@ class SongCreate extends Component {
     this.state = {title: ''};
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
         <h3>Create a New Song</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="song-title-input">Song Title:</label>
           <input
             id="song-title-input"
@@ -23,5 +28,11 @@ class SongCreate extends Component {
     );
   }
 }
+
+const mutation = gql`
+mutation {
+  addSong(title: ) {
+}
+`;
 
 export default SongCreate;
