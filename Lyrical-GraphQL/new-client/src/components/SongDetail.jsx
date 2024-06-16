5af6ba1f4b9ff7d51f5e19fbf72adc0a83d20b64
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-06-16 14:33:18 +0300

    new: Integrating React Router with GraphQL

--- Lyrical-GraphQL/new-client/src/components/SongDetail.jsx
+++ Lyrical-GraphQL/new-client/src/components/SongDetail.jsx
@@ -1,4 +1,13 @@
+import {useQuery} from '@apollo/client';
+import fetchSong from '../queries/fetchSong';
+import {useParams} from 'react-router-dom';
+
 export default function SongDetail() {
+  const {id} = useParams();
+  const {data} = useQuery(fetchSong, {variables: {id}});
+
+  console.log('data:', data);
+
   return (
     <div>
       <h3>Song Detail</h3>
