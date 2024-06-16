88866039e1a52219fc82fa363a00183126006371
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-06-16 17:05:19 +0300

    new: Showing a List of Lyrics

---
+++ Lyrical-GraphQL/new-client/src/components/LyricList.jsx
@@ -0,0 +1,3 @@
+export default function LyricList() {
+  return <ul>LyricList</ul>;
+}
--- Lyrical-GraphQL/new-client/src/components/SongDetail.jsx
+++ Lyrical-GraphQL/new-client/src/components/SongDetail.jsx
@@ -2,6 +2,7 @@ import {useQuery} from '@apollo/client';
 import fetchSong from '../queries/fetchSong';
 import {Link, useParams} from 'react-router-dom';
 import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 export default function SongDetail() {
   const {id} = useParams();
@@ -15,6 +16,7 @@ export default function SongDetail() {
     <div>
       <Link to="/">Back</Link>
       <h3>{song.title}</h3>
+      <LyricList />
       <LyricCreate songId={id} />
     </div>
   );
