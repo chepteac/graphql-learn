afad2e9e9e7f15f7b9353b8661caef96259d4894
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-06-16 15:52:05 +0300

    old: The CreateLyric Mutation

--- Lyrical-GraphQL/client/components/LyricCreate.js
+++ Lyrical-GraphQL/client/components/LyricCreate.js
@@ -1,14 +1,40 @@
 import React, {Component} from 'react';
+import gql from 'graphql-tag';
 
 class LyricCreate extends Component {
+  constructor(props) {
+    super();
+    this.state = {content: ''};
+  }
+
+  submitHandler(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.submitHandler.bind(this)}>
         <label htmlFor="add-lyric">Add a Lyric</label>
-        <input id="add-lyric" />
+        <input
+          id="add-lyric"
+          value={this.state.content}
+          onChange={event => this.setState({content: event.target.value})}
+        />
       </form>
     );
   }
 }
+
+const mutation = gql`
+  mutation AddLyricToSong($content: String, $songId: ID) {
+    addLyricToSong(content: $content, songId: $songId) {
+      id
+      lyrics {
+        id
+        content
+      }
+    }
+  }
+`;
 
 export default LyricCreate;
