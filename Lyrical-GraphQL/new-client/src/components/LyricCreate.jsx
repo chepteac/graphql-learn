3cf0e43e15ee96158137bfafc712c24427895c04
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-06-16 16:34:34 +0300

    new: The CreateLyric Mutation

--- Lyrical-GraphQL/new-client/src/components/LyricCreate.jsx
+++ Lyrical-GraphQL/new-client/src/components/LyricCreate.jsx
@@ -1,8 +1,33 @@
+import {useState} from 'react';
+import {gql} from '@apollo/client';
+
 export default function LyricCreate() {
+  const [content, setContent] = useState('');
+
+  const submitHandler = event => {
+    event.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <label htmlFor="add-lyric">Add a Lyric</label>
-      <input id="add-lyric" />
+      <input
+        id="add-lyric"
+        value={content}
+        onChange={event => setContent(event.target.value)}
+      />
     </form>
   );
 }
+
+const mutation = gql`
+  mutation AddLyricToSong($content: String, $songId: ID) {
+    addLyricToSong(content: $content, songId: $songId) {
+      id
+      lyrics {
+        id
+        content
+      }
+    }
+  }
+`;
