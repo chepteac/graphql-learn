0d8d4247dfe34344d53295202ab1ae6dbebe92dc
Cătălin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2024-08-01 12:59:56 +0300

    new: Showing Likes with Lyrics

--- Lyrical-GraphQL/new-client/src/components/LyricList.jsx
+++ Lyrical-GraphQL/new-client/src/components/LyricList.jsx
@@ -1,27 +1,30 @@
+import {gql, useMutation} from '@apollo/client';
+
 export default function LyricList({lyrics}) {
+  const [likeLyric] = useMutation(gql`
+    mutation LikeLyric($id: ID) {
+      likeLyric(id: $id) {
+        id
+        likes
+      }
+    }
+  `);
+
   const likeHandler = id => {
-    console.log(id);
+    likeLyric(id);
   };
 
   return (
     <ul className="collection">
-      {lyrics.map(({id, content}) => (
+      {lyrics.map(({id, content, likes}) => (
         <li key={id} className="collection-item">
           {content}
           <i className="material-icons" onClick={() => likeHandler(id)}>
             thumb_up
           </i>
+          {likes}
         </li>
       ))}
     </ul>
   );
 }
-
-const mutation = gql`
-  mutation LikeLyric($id: ID) {
-    likeLyric(id: $id) {
-      id
-      likes
-    }
-  }
-`;
