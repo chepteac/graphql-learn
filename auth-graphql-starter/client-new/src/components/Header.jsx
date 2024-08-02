430ea1e76ab4d9c03e59ce119267c8130ae28b93
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-08-02 18:05:05 +0300

    new: Login and Logout Buttons

--- auth-graphql-starter/client-new/src/components/Header.jsx
+++ auth-graphql-starter/client-new/src/components/Header.jsx
@@ -1,24 +1,37 @@
 import {useQuery} from '@apollo/client';
 import query from '../queries/current-user';
+import {Link} from 'react-router-dom';
 
 export default function Header() {
   const {data, loading} = useQuery(query);
 
-  const renderButtons = () => {
-    if (loading) {
-      return <div />;
-    }
-
-    if (data.user) {
-      return <div>Logout</div>;
-    } else {
-      return <div>You are not signed in.</div>;
-    }
-  };
-
   return (
     <nav>
-      <div className="nav-wrapper">{renderButtons()}</div>
+      <div className="nav-wrapper">
+        <Link to="/" className="brand-logo left">
+          Home
+        </Link>
+        {
+          <ul className="right">
+            <Buttons loading={loading} user={data?.user} />
+          </ul>
+        }
+      </div>
     </nav>
+  );
+}
+
+function Buttons({loading, user}) {
+  if (loading) return <div />;
+  if (user) return <div>Logout</div>;
+  return (
+    <div>
+      <li>
+        <Link to="/signup">Signup</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </div>
   );
 }
