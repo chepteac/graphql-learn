4f641f0069466fa83abcc49ad88485ff755c65e5
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-08-02 18:26:38 +0300

    old: Handling the Logout Mutation

--- auth-graphql-starter/client/components/Header.js
+++ auth-graphql-starter/client/components/Header.js
@@ -2,8 +2,13 @@ import React, {Component} from 'react';
 import {graphql} from 'react-apollo';
 import {Link} from 'react-router';
 import query from '../queries/CurrentUser';
+import mutation from '../mutations/Logout';
 
 class Header extends Component {
+  onLogoutClick() {
+    this.props.mutate({});
+  }
+
   renderButtons() {
     const {loading, user} = this.props.data;
 
@@ -12,7 +17,11 @@ class Header extends Component {
     }
 
     if (user) {
-      return <div>Logout</div>;
+      return (
+        <li>
+          <a onClick={this.onLogoutClick.bind(this)}>Logout</a>
+        </li>
+      );
     } else {
       return (
         <div>
@@ -34,11 +43,11 @@ class Header extends Component {
           <Link to="/" className="brand-logo left">
             Home
           </Link>
-          {<ul className="right">{this.renderButtons()}</ul>}
+          <ul className="right">{this.renderButtons()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(graphql(query)(Header));
