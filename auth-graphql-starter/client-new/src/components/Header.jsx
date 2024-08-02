de048063cfcc230b246452fff0a90c0b26e93cea
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-08-02 14:19:41 +0300

    new: Figuring Out the Current User

--- auth-graphql-starter/client-new/src/components/Header.jsx
+++ auth-graphql-starter/client-new/src/components/Header.jsx
@@ -1,3 +1,9 @@
+import {useQuery} from '@apollo/client';
+import query from '../queries/current-user';
+
 export default function Header() {
+  const {data} = useQuery(query);
+  console.log('data:', data);
+
   return <div>Header</div>;
 }
