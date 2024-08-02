b67f696e52281add66ba79b5cc12fbfedf54b083
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-08-02 20:31:43 +0300

    old: Submitting the Auth Form

--- auth-graphql-starter/client/components/AuthForm.js
+++ auth-graphql-starter/client/components/AuthForm.js
@@ -6,13 +6,18 @@ class AuthForm extends Component {
     this.state = {email: '', password: ''};
   }
 
+  onSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state);
+  }
+
   render() {
     return (
       <div className="row">
-        <form className="col s6">
+        <form onSubmit={this.onSubmit.bind(this)} className="col s6">
           <div className="input-field">
-            <label>Email</label>
             <input
+              placeholder="Email"
               value={this.state.email}
               onChange={e => {
                 this.setState({email: e.target.value});
@@ -20,8 +25,9 @@ class AuthForm extends Component {
             />
           </div>
           <div className="input-field">
-            <label>Password</label>
             <input
+              placeholder="Password"
+              type="password"
               value={this.state.password}
               onChange={e => {
                 this.setState({password: e.target.value});
