53834d9cf3e8c62c262caae12c4271c31a785f33
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-08-02 11:57:20 +0300

    new: Root Routes with React Router

--- auth-graphql-starter/client-new/src/App.jsx
+++ auth-graphql-starter/client-new/src/App.jsx
@@ -1,7 +1,14 @@
+import {Outlet} from 'react-router';
+import Header from './components/Header';
 import './index.css';
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
