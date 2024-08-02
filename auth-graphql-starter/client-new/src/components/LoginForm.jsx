a2cdd8fb51e9331a44eab0e4c644a3dc50f2f43f
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-08-02 20:05:22 +0300

    new: Importing the Login Mutation

--- auth-graphql-starter/client-new/src/components/LoginForm.jsx
+++ auth-graphql-starter/client-new/src/components/LoginForm.jsx
@@ -1,6 +1,10 @@
 import AuthForm from './AuthForm';
+import {useMutation} from '@apollo/client';
+import loginMutation from '../mutations/login';
 
 export default function LoginForm() {
+  const [login] = useMutation(loginMutation);
+
   return (
     <div>
       <h3>Login</h3>
