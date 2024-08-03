ceabe5cdc354879c6a0922b5cb192d87bce7f3e0
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-08-03 09:51:54 +0300

    new: Fixing the Login Process

--- auth-graphql-starter/client-new/src/components/LoginForm.jsx
+++ auth-graphql-starter/client-new/src/components/LoginForm.jsx
@@ -1,12 +1,22 @@
 import AuthForm from './AuthForm';
-import {useMutation} from '@apollo/client';
+import {useMutation, useQuery} from '@apollo/client';
 import loginMutation from '../mutations/login';
 import query from '../queries/current-user';
-import {useState} from 'react';
+import {useRef, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 export default function LoginForm() {
+  const navigate = useNavigate();
   const [errors, setErrors] = useState([]);
   const [login] = useMutation(loginMutation);
+  const {data, loading} = useQuery(query);
+  const prevUser = useRef(data.user);
+
+  if (!prevUser.current && data.user && !loading) {
+    navigate('/dashboard');
+  }
+
+  prevUser.current = data.user;
 
   return (
     <div>
