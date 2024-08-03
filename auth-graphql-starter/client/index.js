ebbdfdfb5d5c2c6a29f200b1f193b21ea1a8225d
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-08-03 08:52:31 +0300

    old: More on the Signup Mutation

--- auth-graphql-starter/client/index.js
+++ auth-graphql-starter/client/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient, {createNetworkInterface} from 'apollo-client';
 import {ApolloProvider} from 'react-apollo';
-import {Router, hashHistory, Route, IndexRoute} from 'react-router';
+import {Router, hashHistory, Route} from 'react-router';
 
 import App from './components/App';
 import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
 
 const networkInterface = createNetworkInterface({
   uri: '/graphql',
@@ -25,6 +26,7 @@ const Root = () => {
       <Router history={hashHistory}>
         <Route path="/" component={App}>
           <Route path="/login" component={LoginForm} />
+          <Route path="/signup" component={SignupForm} />
         </Route>
       </Router>
     </ApolloProvider>
